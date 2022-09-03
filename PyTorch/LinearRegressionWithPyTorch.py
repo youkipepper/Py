c8f57c179e51a9f024819a47f188abcb54b926a5
Checkboxy <60711810+youkipepper@users.noreply.github.com>
2022-09-03 22:41:29 +0800

    -

--- PyTorch/LinearRegressionWithPyTorch.py
+++ PyTorch/LinearRegressionWithPyTorch.py
@@ -1,16 +1,15 @@
 import torch
 
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearModel.self).__init__()
+        self.linear = torch.nn.Linear(1, 1)  # 构造对象
 
-# class LinearModel(torch.nn.Module):
-#     def __init__(self):
-#         super(LinearModel.self).__init__()
-#         self.linear = torch.nn.Linear(1, 1)  # 构造对象
+    def forward(self, x):
+        y_pred = self.linear(x)  # 可调用对象
+        return y_pred
 
-#     def forward(self, x):
-#         y_pred = self.linear(x)  # 可调用对象
-#         return y_pred
-
-# model = LinearModel()
+model = LinearModel()
 
 class Foobar:
     def __init__(self):
@@ -21,10 +20,11 @@ class Foobar:
 
 
 foobar = Foobar()
+foobar(1, 2, 3)
 
 
 # def func(*args, **kwargs):
 #     print(args)
 #     print(kwargs)
 
-func(1, 2, 3, x=3, y=5)
+#func(1, 2, 3, x=3, y=5)
