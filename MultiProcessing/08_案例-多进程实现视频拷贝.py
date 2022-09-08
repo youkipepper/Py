0c98e053f73d56becccc1f24b87e459b581ad0f2
Checkboxy <60711810+youkipepper@users.noreply.github.com>
2022-09-08 17:08:45 +0800

    -

--- MultiProcessing/08_案例-多进程实现视频拷贝.py
+++ MultiProcessing/08_案例-多进程实现视频拷贝.py
@@ -2,16 +2,27 @@ import os
 import multiprocessing
 
 
-def copy_file(file_name, souce_dir, dest_dir):
+def copy_file(file_name, source_dir, dest_dir):
     pass
     # 1. 拼接源文件路径和目标文件路径
+    source_path = source_dir + "/" + file_name
+    dest_path = dest_dir + "/" + file_name
+
     # 2. 打开源文件和目标文件
-    # 3. 循环读取源文件到目标路径
+    with open(source_path, "rb") as source_file:
+        with open(dest_path, "wb")as dest_file:
+            # 3. 循环读取源文件到目标路径
+            while True:
+                data = source_file.read(1024)
+                if data:
+                    dest_file.write(data)
+                else:
+                    break
 
 
 if __name__ == '__main__':
     # 1. 定义源文件夹和目标文件夹
-    source_dir = "PythonTeachVideo"
+    source_dir = "/Users/youkipepper/Desktop/Py/MultiProcessing/PythonTeachVideo"
     dest_dir = "/Users/youkipepper/Desktop/Py/MultiProcessing/08_test"
 
     # 2. 创建目标文件夹
@@ -30,5 +41,5 @@ if __name__ == '__main__':
         # 5. 使用多进程实现多任务拷贝
         sub_process = multiprocessing.Process(
             target=copy_file, args=(file_name, source_dir, dest_dir))
-            
+
         sub_process.start()
