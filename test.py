d77a3b7a7d93a2023f3a6b8a561798d25b5c216f
youki <youkipepper@gmail.com>
2022-11-05 11:21:03 +0800

    none

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import urllib.request
-
-url = "https://www.baidu.com"
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-}
-
-request = urllib.request.Request(url=url, headers=headers)
-
-response = urllib.request.Request(request)
-
-content = response.read().decode('utf8')
-
-print(content)
+print("hello world!")
